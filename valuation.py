b95ff013865b51147b681003c888aa67ec71b564
CdRom1 <r.maliach@live.fr>
2019-06-13 17:26:56 +0200

    Valuation locale ok, plus qu'à ajouter un dijkstra

---
+++ valuation.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+#Prend en entrée une grille-hangar et une liste de points imposés. Pour chaque couple de points imposés, calcule un chemin ayant un nombre minimum de virages (1); calcule le coût en temps de ce chemin; puis trouve un chemin passant par tous les points imposés.
+
+import numpy as np;
+from math import sqrt, pi;
+import networkx as nx;
+from itertools import combinations;
+import Draw as d;
+
+grid = np.array([5,5]); #5*5 = 25 intersections, celle en bas à droite s'appelle (24,0).
+corridorWidth = 0.02; #m, utilisé pour calculer le Rc
+
+breakPoints = [(0,0),(4,8),(3,12)];
+
+
+mu = 0.35; #coefficient de frottement statique carton/fer déterminé pour notre prototype
+g = 9.80665; #m*s^-2*g
+r = 0.02; #m, rayon des roues
+e = 0.07; #m, hauteur du centre de gravité (colis)
+l = 0.03; #m, écartement des roues
+rc = corridorWidth/2;
+
+
+def localPath(a,b):
+    #prend en entrée deux points de la grille, retourne le meilleur chemin et son coût
+    #la convention est de toujours prendre le chemin dont le premier segment est horizontal
+    #le chemin est encodé au format sommet
+    path = [];
+    path.append(a);
+    path.append((a[0],b[1]));
+    path.append(b);
+    return [path,straightLineTime(abs(b[0]-a[0])) + straightLineTime(abs(b[1] - a[1])) + bendTime(rc)];
+
+def straightLineTime(d):
+    #temps minimal pour parcourir une belle ligne droite de longueur d
+    return 2*sqrt(d/(mu*g));
+
+def bendTime(rc):
+    #temps minimal pour parcourir un virage de rayon de courbure rc
+    return pi*sqrt(rc*(r+e)/(2*l*g));
+
+def breakDownGrid(grid,breakPoints):
+    #retourne un graphe fortement connexe dont les sommets sont les points d'arrêt pour chaque couple de points imposés, chaque arête a pour poids le coût du chemin optimal local, le tracé dans la grille du chemin optimal local est stocké dans l'attribut localPath
+    G = nx.Graph();
+    G.add_nodes_from(breakPoints);
+    pairs = list(combinations(breakPoints,2)); #very bad code
+    for pair in pairs:
+        path = localPath(pair[0],pair[1]);
+        G.add_edge(pair[0],pair[1],weight=path[1],localPath=path[0]); #localPath est un attribut non-standard de networkx (il n'a pas d'effet de bord, son nom est défini arbitrairement)
+    return G;
+
+
+d.drawNxGraph(breakDownGrid(grid, breakPoints));
